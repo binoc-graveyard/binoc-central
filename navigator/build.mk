7dd1e31c9c07e6599f89d5fb70b82aa550eb5bfb
Matt A. Tobin <email@mattatobin.com>
2022-01-23 18:27:13 -0600

    [Navigator] Prototype GRE Packager

--- navigator/build.mk
+++ navigator/build.mk
@@ -2,17 +2,22 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+MAKE_INSTALLER_TARGET = $(MAKE) -C navigator/installer
+
 installer:
-	@$(MAKE) -C navigator/installer installer
+	@$(MAKE_INSTALLER_TARGET) $@
 
 package:
-	@$(MAKE) -C navigator/installer make-archive
+	@$(MAKE_INSTALLER_TARGET) archive
 
-mozpackage:
-	@$(MAKE) -C navigator/installer make-package
+buildsymbols:
+	@$(MAKE_INSTALLER_TARGET) symbols
+
+l10n-package:
+	@$(MAKE_INSTALLER_TARGET) locale
+
+theme-package:
+	@$(MAKE_INSTALLER_TARGET) skin
 
 stage-package:
-	@$(MAKE) -C navigator/installer stage-package make-buildinfo-file
-
-install::
-	@$(MAKE) -C navigator/installer install
+	@$(MAKE_INSTALLER_TARGET) $@
